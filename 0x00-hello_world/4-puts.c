5da5107193cb6afd0280411ad72527578a8746d3
Mary-Katherine <andamary2020@gmail.com>
2023-02-16 18:48:17 -0800

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - this is the main function
+ * main - this is my main function
  *
- * Return: always 0
+ * Return: 0 when successful
  */
 
 int main(void)
 {
-             puts("\"Programming is like building a multilingual puzzle");
-	     return (0);
-} 	
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
