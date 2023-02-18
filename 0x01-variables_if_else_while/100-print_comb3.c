57f40e96b3cbb1c7f0211ef2b7e821b57566b4a6
Mary-Katherine <andamary2020@gmail.com>
2023-02-18 12:32:30 -0800

    Print comb 2

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	int i;
+	int n;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (n = 1 ; n < 10 ; n++)
+		{
+			if (i < n && i != n)
+			{
+				putchar(i + '0');
+				putchar(n + '0');
+				if (i + n != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
