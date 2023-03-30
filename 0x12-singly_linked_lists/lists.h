03d8bc0687b4c5fa2ca17d8e8db78b60c0b7e487
Mary-Katherine <andamary2020@gmail.com>
2023-03-30 15:36:10 -0700

    lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,8 +1,13 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef LISTS_H
+#define LISTS_H
 
-#include <stdio.h>
-#include <string.h>
+/*
+ * File: lists.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file containing prototypes and definitions for all functions
+ *       and types written in the 0x11-singly_linked_lists directory.
+ */
+
 #include <stdlib.h>
 
 /**
@@ -12,20 +17,25 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
-*/
+ *              for ALX project
+ */
+
 typedef struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next;
+
 } list_t;
 
-
 size_t print_list(const list_t *h);
+
 size_t list_len(const list_t *h);
+
 list_t *add_node(list_t **head, const char *str);
+
 list_t *add_node_end(list_t **head, const char *str);
+
 void free_list(list_t *head);
 
-#endif /* LIST_H */
+#endif /* LISTS_H */
